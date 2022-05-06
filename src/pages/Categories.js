4d8c52d7e0709282d714b7f9482e09f1c44ed529
amrendrakind <vision2010@gmail.com>
2022-05-06 23:23:14 +0530

    Update Pages and App.js

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -7,4 +7,5 @@ function Categories() {
     </div>
   );
 }
+
 export default Categories;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddBook from '../componenets/Books/AddBook';
+import BookList from '../componenets/Books/BookList'
+
+const Home = () => (
+    <>
+      <BookList />
+      <AddBook />
+    </>
+  );
+  
+  export default Home;
+
