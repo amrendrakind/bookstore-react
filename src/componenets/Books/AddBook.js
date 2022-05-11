f66d93bf18e92775e31d801d0e01041af54357f9
amrendrakind <vision2010@gmail.com>
2022-05-12 01:17:29 +0530

    Update AddBook functionality

--- src/componenets/Books/AddBook.js
+++ src/componenets/Books/AddBook.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../../redux/books/books';
 
 function AddBook() {
   const [inputText, setInputText] = useState({ title: '', author: '' });
+
+  const dispatch = useDispatch();
 
   const onChange = (e) => {
     setInputText({
@@ -11,6 +16,19 @@ function AddBook() {
   };
   const submitHandler = (e) => {
     e.preventDefault();
+
+    const newBook = {
+      id: uuidv4(),
+      title: inputText.title,
+      author: inputText.author,
+    };
+
+    dispatch(addBook(newBook));
+
+    setInputText({
+      title: '',
+      author: '',
+    });
   };
   return (
     <>
@@ -23,6 +41,7 @@ function AddBook() {
           value={inputText.title}
           name="title"
           onChange={onChange}
+          required
         />
         <input
           type="text"
@@ -31,6 +50,7 @@ function AddBook() {
           value={inputText.author}
           name="author"
           onChange={onChange}
+          required
         />
         <button type="submit"> Add Book</button>
       </form>
