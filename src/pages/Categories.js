a680dd754b129810d90aa8befc9cca8cf5f9d748
amrendrakind <vision2010@gmail.com>
2022-05-12 03:24:33 +0530

    Update Redux implementation in Categories

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 function Categories() {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.Categories);
+  const handleStatus = () => {
+    dispatch(checkStatus());
+  };
+
   return (
     <div>
-      <button type="button">Check status</button>
+      <button type="button" onClick={handleStatus}>Check status</button>
+      <br />
+      {categories}
     </div>
   );
 }
