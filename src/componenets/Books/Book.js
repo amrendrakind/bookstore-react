4a0016fdfdf9afdf822c05d3932651e636436b4b
amrendrakind <vision2010@gmail.com>
2022-05-11 06:59:01 +0530

    Update redux in Book.js

--- src/componenets/Books/Book.js
+++ src/componenets/Books/Book.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Book.css';
+import { useDispatch } from 'react-redux';
+import { REMOVE_BOOK } from '../../redux/types';
 
 function Book(props) {
+  const dispatch = useDispatch();
   const { book } = props;
   const { id, author, title } = book;
+
+  const removeBook = (id) => ({
+    type: REMOVE_BOOK,
+    payload: id,
+  });
+
+  const handleDispatch = (id) => {
+    dispatch(removeBook(id));
+  };
 
   return (
 
@@ -14,7 +26,7 @@ function Book(props) {
       {author}
 
       <span>
-        <button type="button">Remove</button>
+        <button type="button" onClick={handleDispatch}>Remove</button>
       </span>
     </li>
   );
