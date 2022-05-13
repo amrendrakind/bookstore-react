e2a31bc812f08cdd41163bc229dd7814abd18bca
amrendrakind <vision2010@gmail.com>
2022-05-14 03:22:16 +0530

    Update styling Book

--- src/componenets/Books/Book.js
+++ src/componenets/Books/Book.js
@@ -41,7 +41,23 @@ function Book(props) {
               <button className="action-button" type="button">Edit</button>
             </div>
           </div>
-
+          <div className="progress-container">
+            <div className="circular-progress-container">
+              <div className="circular-progress" />
+            </div>
+            <div className="progress-statistics">
+              <p className="percent-complete">64%</p>
+              <p className="completed">Completed</p>
+            </div>
+            <div className="progress-divider" />
+          </div>
+          <div className="current-chapter-container">
+            <div>
+              <p className="current-chapter-label">CURRENT CHAPTER</p>
+              <p className="current-chapter-number">CHAPTER 17</p>
+            </div>
+            <button className="update-progress-button" type="button">UPDATE PROGRESS</button>
+          </div>
         </div>
       </div>
     </li>
